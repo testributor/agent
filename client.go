4840217ce697a1666c0481fbaa7069d399d6d382
Dimitris Karakasilis <jimmykarily@gmail.com>
2016-07-08 16:26:53 +0300

    Let all APICLient instances use the same token to avoid extra calls
    This way, each thread can have its own APIClient (to avoid blockings
    between threads) but still avoid making one token request per thread.

--- client.go
+++ client.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/clientcredentials"
 	"io/ioutil"
 	"net/http"
@@ -15,7 +16,16 @@ var testributorUrl = os.Getenv("TESTRIBUTOR_URL")
 var apiUrl string
 var appID = os.Getenv("APP_ID")
 var appSecret = os.Getenv("APP_SECRET")
-var conf *clientcredentials.Config
+
+// We build only one tokenSource and use it to create new APIClients
+// (through NewClient() function) to avoid making multiple requests for
+// token generation.
+// oauth2.TokenSource is an interface which if implemented in our case by a
+// *oauth2.reuseTokenSource (Use fmt.Printf("%T\n", tokenSource) to see that).
+// Since this is a pointer, only the first call to
+// NewClient() will perform a token request. After that the token will be reused
+// since it points to an already initialized TokenSource value (with a valid token).
+var tokenSource oauth2.TokenSource
 
 func init() {
 	// Default url if environment var is not set
@@ -33,17 +43,18 @@ func init() {
 		os.Exit(1)
 	}
 
-	conf = &clientcredentials.Config{
+	conf := &clientcredentials.Config{
 		ClientID:     appID,
 		ClientSecret: appSecret,
 		//Scopes:       []string{"SCOPE1", "SCOPE2"},
 		TokenURL: testributorUrl + "oauth/token",
 	}
+	tokenSource = conf.TokenSource(context.Background())
 }
 
 func NewClient() *APIClient {
 	return &APIClient{
-		*conf.Client(context.Background()),
+		*oauth2.NewClient(context.Background(), tokenSource),
 	}
 }
 

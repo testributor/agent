d1c108b0ab1dbe27f770d8ecfd101c834f769b59
Dimitris Karakasilis <jimmykarily@gmail.com>
2016-07-14 15:54:48 +0300

    Print logo on startup

--- agent.go
+++ agent.go
@@ -4,6 +4,9 @@ import "os"
 
 func main() {
 	logger := Logger{"Main", os.Stdout}
+
+	printLogo(logger)
+
 	// Check if env vars are set, use defaults if not (or exit if needed)
 	// and initialize oauth token.
 	err := SetupClientData()
@@ -28,4 +31,16 @@ func main() {
 	go worker.Start()
 	go reporter.Start()
 	manager.Start()
+}
+
+// Because we can
+func printLogo(logger Logger) {
+	logger.Log(`
+ _______________________  _______  __  ____________  ___
+/_  __/ __/ __/_  __/ _ \/  _/ _ )/ / / /_  __/ __ \/ _ \
+ / / / _/_\ \  / / / , _// // _  / /_/ / / / / /_/ / , _/
+/_/ /___/___/ /_/ /_/|_/___/____/\____/ /_/  \____/_/|_|
+
+                              https://www.testributor.com
+`)
 }

fa1695acf3fbb8253ece0182de52956a256204db
Dimitris Karakasilis <jimmykarily@gmail.com>
2016-07-27 10:46:45 +0300

    Fallback to /bin/sh when $SHELL is not set
    as is the case when running the agent on Debian jessie docker image.
    /bin/sh should always be there.

--- system_command/posix_command.go
+++ system_command/posix_command.go
@@ -8,5 +8,9 @@ import (
 // Runs a command on the Windows shell
 func PosixShellCommand(command string) *exec.Cmd {
 	shell := os.Getenv("SHELL")
+	// If SHELL is not set, just use /bin/sh
+	if shell == "" {
+		shell = "/bin/sh"
+	}
 	return exec.Command(shell, "-c", command)
 }
